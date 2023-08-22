c3dd18da423de4864508631dcce1e02b76a06e6c
ValeriiaMezentseva <99216072+ValeriiaMezentseva@users.noreply.github.com>
2023-08-22 10:08:48 +0000

    Deploying to gh-pages from @ ValeriiaMezentseva/my-portfolio@20286c6b435bd4ba92aea5c086c0e0df52ae6ddf 🚀

---
+++ static/js/71.2384f0a7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[71],{71:function(t,a,e){e.d(a,{GYo:function(){return n}});var r=e(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M424 80H88a56.06 56.06 0 00-56 56v240a56.06 56.06 0 0056 56h336a56.06 56.06 0 0056-56V136a56.06 56.06 0 00-56-56zm-14.18 92.63l-144 112a16 16 0 01-19.64 0l-144-112a16 16 0 1119.64-25.26L256 251.73l134.18-104.36a16 16 0 0119.64 25.26z"}}]})(t)}}}]);
+//# sourceMappingURL=71.2384f0a7.chunk.js.map
